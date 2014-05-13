193a03917b78bbfef860433a667c7fba4b148cea
Mr.lovable <841554024@qq.com>
2014-05-13 16:52:14 +0800

    my first commit

---
+++ HelloWord/HelloWordTests/HelloWordTests.m
@@ -0,0 +1,34 @@
+//
+//  HelloWordTests.m
+//  HelloWordTests
+//
+//  Created by mac on 14-5-13.
+//  Copyright (c) 2014年 mac. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HelloWordTests : XCTestCase
+
+@end
+
+@implementation HelloWordTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
